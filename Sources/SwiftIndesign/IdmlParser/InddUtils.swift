dc102ef6c402a8e93685f5c9d67d33669a3649a6
JinYuchyi <yuqi_jin@apple.com>
2023-12-15 17:22:51 +0800

    Debug on convert to idml function.

--- Sources/SwiftIndesign/IdmlParser/InddUtils.swift
+++ Sources/SwiftIndesign/IdmlParser/InddUtils.swift
@@ -11,10 +11,14 @@ class InddUtils {
     // If targetIdmlPath is nil, export to same path
     static func convertToIdml(inddPath: String, targetIdmlPath: String?, targetFolder: String?) {
         if targetIdmlPath == nil && targetFolder == nil {
-
+            return
         }
         let script = AppleScript.saveToIdml(indd: inddPath, targetIdmlPath: targetIdmlPath, targetFolder: targetFolder)
-        let _ = try? ScriptUtils.runShell(command: "osascript -e '\(script)'")
+        let scriptCmd = "osascript -e '\(script)'"
+        #if DEBUG
+        print(scriptCmd)
+        #endif
+        let _ = try? ScriptUtils.runShell(command: scriptCmd)
         return
     }
 
